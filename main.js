e776ea88a9f1be4f4ab5a11176dcf0abf45fd22a
TonSupachaiMisa <supachaimisa@gmail.com>
2023-11-29 11:47:16 +0700

    add just cosmatic

--- main.js
+++ main.js
@@ -13,24 +13,60 @@ const io = socketIO(server);
 // Serve static files from the 'public' directory
 app.use(express.static(__dirname + '/public'));
 
-const userUid = uuidV4();
+const users = [
+    // { id: "uuid", socketId: "", userName: "xxx" }
+];
+
 // Socket.io connection handling
 io.on('connection', (socket) => {
+    const userUid = uuidV4();
+    // console.log(`${userUid} user connected`);
+    // console.log(`${socket.id} user connected`);
 
-  console.log(`${userUid} user connected`);
-
-  // Listen for chat messages
-  socket.on('chat message', (msg) => {
-    console.log('msg: ', msg);
-    io.emit('chat message', msg); // Broadcast the message to all connected clients
-  });
-
-  // Handle disconnection
-  socket.on('disconnect', () => {
+    function createUser(){
+        users.push(
+            { id: userUid, socketId: socket.id, userName: null }
+        );
+    }
+    createUser();
     
-    console.log(`${userUid} user disconnected`);
-  });
+    function findUser(id){
+        const resUser = users.find(user => user.id === id);
+        return resUser;
+    }
+
+    function setNameUser(id, name){
+        const idxUser = users.findIndex(user => user.id === id);
+        users[idxUser].userName = name;
+        return users[idxUser];
+    }
+
+    function getNameUser(id){
+        const resUser = users.find(user => user.id === id);
+        return resUser.userName;
+    }
+
+    // Listen for chat messages
+    socket.on('chat message', (msg) => {
+        const user = findUser(userUid)
+        if(!user?.userName){
+            setNameUser(userUid, msg.userName);
+        }
+
+        // userInfo
+        console.log('user: ', user);
+        io.emit('chat message', msg); // Broadcast the message to all connected clients
+    });
+
+    // Handle disconnection
+    socket.on('disconnect', () => {
+        console.log(`${userUid} user disconnected`);
+    });
 });
+
+// app.get('/', (req, res) =>{
+
+// });
 
 // Start the server
 const PORT = process.env.PORT || 3000;
